33447e3b03ad33b2d0104317a407ac014beb9e63
xmsk <23740884+xmsk@users.noreply.github.com>
2020-01-28 17:13:32 +0100

    replace two-way binding with one-way binding because it is easier

--- src/PlayerView.tsx
+++ src/PlayerView.tsx
@@ -15,30 +15,28 @@ import {
 } from 'tabris';
 import {
     component,
-    property
+    property,
 } from 'tabris-decorators';
 import * as fonts from './fonts';
-import {PlayerViewModel} from './PlayerViewModel';
+import { Model } from './models/Model';
+import { Player } from './models/Player';
+import { PlayerStats } from './models/PlayerStats';
+import { View } from './views/View';
 
 @component // Enabled data binding syntax
-export class PlayerView extends Composite {
+export class PlayerView extends Composite implements View {
 
-    @property public model: PlayerViewModel;
+    @property public player: Player;
+    @property public stats: PlayerStats;
 
     // placeholder for where the image can be in the future
-    private _image: Composite;
+    // private _image: Composite;
 
     constructor(properties: Properties<PlayerView>) {
         super();
 
         // need to set properties before we reference them (e.g. the model)
         this.set(properties);
-        this.model.onPlayerChanged(
-            () => this._redraw()
-        );
-        this.model.onStatsChanged(
-            () => this._redraw()
-        );
 
         // this._image = new Composite({
         //     layoutData: 'stretchX',
@@ -51,41 +49,47 @@ export class PlayerView extends Composite {
             // this._image,
             <$>
                 <Composite stretchX height={300} top={0} background='linear-gradient(45deg, #0288d1 10%, #00dfff)'/>
-                <Composite id='playerName' stretchX top='prev() 8' height={35}>
-                    <TextView id='Name' left={16} width={150} font={fonts.medium}>{this.model.player.FirstName + " " + this.model.player.LastName}</TextView>
+                <Composite id='PlayerName' stretchX top='prev() 8' height={35}>
+                    <TextView id='FirstName' left={16} width={75} font={fonts.medium} bind-text='player.FirstName'/>
+                    <TextView id='LastName' left='prev() 8' width={75} font={fonts.medium} bind-text='player.LastName'/>
                 </Composite>
                 <Composite id='stats' stretch top='prev() 8' bottom='next()'>
                     <Composite id='Games' stretchX top={0} height={35}>
                         <TextView id='gmLabel' left={16} width={150} font={fonts.medium}>Games: </TextView>
-                        <TextView id='gmNumber' left='prev() 8' width={50} font={fonts.medium}>{this.model.stats.Games}</TextView>
+                        <TextView id='gmNumber' left='prev() 8' width={50} font={fonts.medium} bind-text='stats.Games'/>
                     </Composite>
                     <Composite id='touchdowns' stretchX top='prev() 8' height={35}>
                         <TextView id='tdLabel' left={16} width={150} font={fonts.medium}>Touchdowns: </TextView>
-                        <TextView id='tdNumber' left='prev() 8' width={50} font={fonts.medium}>{this.model.stats.Touchdowns}</TextView>
+                        <TextView id='tdNumber' left='prev() 8' width={50} font={fonts.medium} bind-text='stats.Touchdowns'/>
                     </Composite>
                     <Composite id='onePointTrys' stretchX top='prev() 8' height={35}>
                         <TextView id='optLabel' left={16} width={150} font={fonts.medium}>One Point Trys: </TextView>
-                        <TextView id='optNumber' left='prev() 8' width={50} font={fonts.medium}>{this.model.stats.OnePointTrys}</TextView>
+                        <TextView id='optNumber' left='prev() 8' width={50} font={fonts.medium} bind-text='stats.OnePointTrys'/>
                     </Composite>
                     <Composite id='twoPointTry' stretchX top='prev() 8' height={35}>
                         <TextView id='tptLabel' left={16} width={150} font={fonts.medium}>Two Point Trys: </TextView>
-                        <TextView id='tptNumber' left='prev() 8' width={50} font={fonts.medium}>{this.model.stats.TwoPointTrys}</TextView>
+                        <TextView id='tptNumber' left='prev() 8' width={50} font={fonts.medium} bind-text='stats.TwoPointTrys'/>
                     </Composite>
                     <Composite id='interceptions' stretchX top='prev() 8' height={35}>
                         <TextView id='intLabel' left={16} width={150} font={fonts.medium}>Interceptions: </TextView>
-                        <TextView id='intNumber' left='prev() 8' width={50} font={fonts.medium}>{this.model.stats.Interceptions}</TextView>
+                        <TextView id='intNumber' left='prev() 8' width={50} font={fonts.medium} bind-text='stats.Interceptions'/>
                     </Composite>
                     <Composite id='sacks' stretchX top='prev() 8' height={35}>
                         <TextView id='sxLabel' left={16} width={150} font={fonts.medium}>Sacks: </TextView>
-                        <TextView id='sxNumber' left='prev() 8' width={50} font={fonts.medium}>{this.model.stats.Sacks}</TextView>
+                        <TextView id='sxNumber' left='prev() 8' width={50} font={fonts.medium} bind-text='stats.Sacks'/>
                     </Composite>
                 </Composite>
             </$>
         );
     }
 
-    private _redraw() {
-        // implement;
+    public setModel(model: Model ) {
+        if (model instanceof Player) {
+            this.player = model;
+        } else if (model instanceof PlayerStats) {
+            this.stats = model;
+        } else {
+            console.error("Unknown type of model", typeof(model));
+        }
     }
-
 }
