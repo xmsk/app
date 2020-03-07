e2d9bc7994e4ceeb05dc7b8b3994ac29ded708d1
xmsk <23740884+xmsk@users.noreply.github.com>
2020-03-07 12:09:52 +0100

    change indentation from 2 to 4 spaces

--- src/views/PlayersView.tsx
+++ src/views/PlayersView.tsx
@@ -7,23 +7,23 @@ implement filter mechanisms to filter the players and possibly allow selecting a
 player to display detailed information
 */
 import {
-  Action,
-  ActivityIndicator,
-  Composite,
-  NavigationView,
-  Page,
-  Popover,
-  Properties,
-  TextView,
-  Widget,
-  WidgetTapEvent,
+    Action,
+    ActivityIndicator,
+    Composite,
+    NavigationView,
+    Page,
+    Popover,
+    Properties,
+    TextView,
+    Widget,
+    WidgetTapEvent,
 } from 'tabris';
 import {
-  Cell,
-  component,
-  List,
-  ListView,
-  property,
+    Cell,
+    component,
+    List,
+    ListView,
+    property,
 } from 'tabris-decorators';
 import { Player } from '../models/Player';
 import { PlayerStats } from '../models/PlayerStats';
@@ -35,95 +35,95 @@ import { Waitable } from './Waitable';
 @component // Enabled data binding syntax
 export class PlayersView extends Page implements ModelListSettable, Waitable {
 
-  @property public players: List<Player> = new List<Player>();
-  private _playersListView: ListView<Player>;
-  private listFont: fonts.CustomFont = fonts.large;
-  private listHeadingFont: fonts.CustomFont = fonts.largeBold;
-  private waitMode: boolean = false;
+    @property public players: List<Player> = new List<Player>();
+    private _playersListView: ListView<Player>;
+    private listFont: fonts.CustomFont = fonts.large;
+    private listHeadingFont: fonts.CustomFont = fonts.largeBold;
+    private waitMode: boolean = false;
 
-  constructor(properties: Properties<PlayersView>) {
-    super();
+    constructor(properties: Properties<PlayersView>) {
+        super();
 
-    // need to set properties before we reference them (e.g. the model)
-    this.set(properties);
+        // need to set properties before we reference them (e.g. the model)
+        this.set(properties);
 
-    this.append(
-      <$>
-        <ActivityIndicator id='activity' class='waitMode' visible={false} center/>
-        <TextView id='heading' stretchX top={0} height={fonts.largeBold.viewHeight} font={fonts.largeBold} alignment='left'>NFFL Players</TextView>
-        <Composite id='listHeaders' stretchX top='prev()' height={this.listHeadingFont.viewHeight}>
-            <TextView centerY left='prev()' width={this.listHeadingFont.viewHeight} height={this.listHeadingFont.viewHeight} alignment='right' font={this.listHeadingFont} textColor='#212121' text='#'/>
-            <TextView centerY left='prev()' width={this.listHeadingFont.viewHeight} height={this.listHeadingFont.viewHeight} alignment='centerX' font={this.listHeadingFont} textColor='#212121' text=''/>
-            <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='FirstName'/>
-            <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='LastName'/>
-            <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='TeamName'/>
-        </Composite>
-        <ListView id='playersList' class='notWaitMode' stretchX bottom='next()' top='prev()' items={this.players}>
-          <Cell highlightOnTouch onTap={
-            (ev) => {
-              console.log('click player');
-              console.log(ev.target);
-              this._openPlayerPopover(ev);
-            }
-          }>
-            <TextView centerY left='prev()' width={this.listFont.viewHeight} height={this.listFont.viewHeight} alignment='right' font={this.listFont} textColor='#212121' bind-text='item.JerseyNumber'/>
-            <TextView centerY left='prev()' width={this.listFont.viewHeight} height={this.listFont.viewHeight} alignment='centerX' font={this.listFont} textColor='#212121' text='-'/>
-            <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.FirstName'/>
-            <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.LastName'/>
-            <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.Team.TeamName'/>
-          </Cell>
-        </ListView>
-      </$>
-    );
-    // have to enable waitMode after we have created the widgets
-    this.toggleWaitMode(true);
-    // assign ListView for easier handling
-    this._playersListView = this._find(ListView).filter('#playersList').only();
-  }
-
-  public setModelList(list: List<Player>): void {
-    this.toggleWaitMode(false);
-    this.players = list;
-    // have to update ListView items because assignment destroys the object?
-    this._playersListView.items = this.players;
-  }
-
-  public toggleWaitMode(state?: boolean): void {
-    if (state === undefined) {
-        this.waitMode = !this.waitMode;
-    } else {
-        this.waitMode = state;
+        this.append(
+            <$>
+                <ActivityIndicator id='activity' class='waitMode' visible={false} center/>
+                <TextView id='heading' stretchX top={0} height={fonts.largeBold.viewHeight} font={fonts.largeBold} alignment='left'>NFFL Players</TextView>
+                <Composite id='listHeaders' stretchX top='prev()' height={this.listHeadingFont.viewHeight}>
+                        <TextView centerY left='prev()' width={this.listHeadingFont.viewHeight} height={this.listHeadingFont.viewHeight} alignment='right' font={this.listHeadingFont} textColor='#212121' text='#'/>
+                        <TextView centerY left='prev()' width={this.listHeadingFont.viewHeight} height={this.listHeadingFont.viewHeight} alignment='centerX' font={this.listHeadingFont} textColor='#212121' text=''/>
+                        <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='FirstName'/>
+                        <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='LastName'/>
+                        <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='TeamName'/>
+                </Composite>
+                <ListView id='playersList' class='notWaitMode' stretchX bottom='next()' top='prev()' items={this.players}>
+                    <Cell highlightOnTouch onTap={
+                        (ev) => {
+                            console.log('click player');
+                            console.log(ev.target);
+                            this._openPlayerPopover(ev);
+                        }
+                    }>
+                        <TextView centerY left='prev()' width={this.listFont.viewHeight} height={this.listFont.viewHeight} alignment='right' font={this.listFont} textColor='#212121' bind-text='item.JerseyNumber'/>
+                        <TextView centerY left='prev()' width={this.listFont.viewHeight} height={this.listFont.viewHeight} alignment='centerX' font={this.listFont} textColor='#212121' text='-'/>
+                        <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.FirstName'/>
+                        <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.LastName'/>
+                        <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.Team.TeamName'/>
+                    </Cell>
+                </ListView>
+            </$>
+        );
+        // have to enable waitMode after we have created the widgets
+        this.toggleWaitMode(true);
+        // assign ListView for easier handling
+        this._playersListView = this._find(ListView).filter('#playersList').only();
     }
 
-    this._find('.waitMode').forEach(
-        (item) => item.visible = this.waitMode
-    );
-    this._find('.notWaitMode').forEach(
-        (item) => item.visible = !this.waitMode
-    );
+    public setModelList(list: List<Player>): void {
+        this.toggleWaitMode(false);
+        this.players = list;
+        // have to update ListView items because assignment destroys the object?
+        this._playersListView.items = this.players;
+    }
+
+    public toggleWaitMode(state?: boolean): void {
+        if (state === undefined) {
+                this.waitMode = !this.waitMode;
+        } else {
+                this.waitMode = state;
+        }
+
+        this._find('.waitMode').forEach(
+                (item) => item.visible = this.waitMode
+        );
+        this._find('.notWaitMode').forEach(
+                (item) => item.visible = !this.waitMode
+        );
 }
 
-  public refreshPlayers() {
-    this.toggleWaitMode(true);
-    Player.listFactory.constructToView("", this, Player);
-  }
+    public refreshPlayers() {
+        this.toggleWaitMode(true);
+        Player.listFactory.constructToView("", this, Player);
+    }
 
-  private _openPlayerPopover(ev: WidgetTapEvent<Widget>) {
-    console.log("function open player popover");
-    const playerIndex = this._playersListView.itemIndex(ev.target);
-    const player = this.players[playerIndex];
-    const playerView = <PlayerView stretch/>;
-    Player.factory.constructToView(player, playerView, Player);
-    PlayerStats.factory.constructToView(player.PlayerId, playerView, PlayerStats);
-    const popover = Popover.open(
-      <Popover>
-        <NavigationView stretch>
-          <Action placement='navigation' title='Close' onSelect={() => popover.close()}/>
-          <Page title={player.Identification}/>
-        </NavigationView>
-      </Popover>
-    );
-    let page = popover.contentView.find('Page').only();
-    playerView.appendTo(page);
-  }
+    private _openPlayerPopover(ev: WidgetTapEvent<Widget>) {
+        console.log("function open player popover");
+        const playerIndex = this._playersListView.itemIndex(ev.target);
+        const player = this.players[playerIndex];
+        const playerView = <PlayerView stretch/>;
+        Player.factory.constructToView(player, playerView, Player);
+        PlayerStats.factory.constructToView(player.PlayerId, playerView, PlayerStats);
+        const popover = Popover.open(
+            <Popover>
+                <NavigationView stretch>
+                    <Action placement='navigation' title='Close' onSelect={() => popover.close()}/>
+                    <Page title={player.Identification}/>
+                </NavigationView>
+            </Popover>
+        );
+        let page = popover.contentView.find('Page').only();
+        playerView.appendTo(page);
+    }
 }
--- src/views/TeamsView.tsx
+++ src/views/TeamsView.tsx
@@ -7,23 +7,23 @@ implement filter mechanisms to filter the teams and allow selecting a team to
 display detailed information
 */
 import {
-  Action,
-  ActivityIndicator,
-  Composite,
-  NavigationView,
-  Page,
-  Popover,
-  Properties,
-  TextView,
-  Widget,
-  WidgetTapEvent,
+    Action,
+    ActivityIndicator,
+    Composite,
+    NavigationView,
+    Page,
+    Popover,
+    Properties,
+    TextView,
+    Widget,
+    WidgetTapEvent,
 } from 'tabris';
 import {
-  Cell,
-  component,
-  List,
-  ListView,
-  property,
+    Cell,
+    component,
+    List,
+    ListView,
+    property,
 } from 'tabris-decorators';
 import { Team } from '../models/Team';
 import { TeamStats } from '../models/TeamStats';
@@ -35,91 +35,91 @@ import { Waitable } from './Waitable';
 @component
 export class TeamsView extends Page implements ModelListSettable, Waitable {
 
-  @property public teams: List<Team> = new List<Team>();
-  private _teamsListView: ListView<Team>;
-  private listFont: fonts.CustomFont = fonts.large;
-  private listHeadingFont: fonts.CustomFont = fonts.largeBold;
-  private waitMode: boolean = false;
+    @property public teams: List<Team> = new List<Team>();
+    private _teamsListView: ListView<Team>;
+    private listFont: fonts.CustomFont = fonts.large;
+    private listHeadingFont: fonts.CustomFont = fonts.largeBold;
+    private waitMode: boolean = false;
 
-  constructor(properties: Properties<TeamsView>) {
-    super();
+    constructor(properties: Properties<TeamsView>) {
+        super();
 
-    // need to set properties before we reference them (e.g. the model)
-    this.set(properties);
+        // need to set properties before we reference them (e.g. the model)
+        this.set(properties);
 
-    this.append(
-      <$>
-        <ActivityIndicator id='activity' class='waitMode' visible={false} center/>
-        <TextView id='heading' stretchX top={0} height={fonts.largeBold.viewHeight} font={fonts.largeBold} alignment='left'>NFFL Teams</TextView>
-        <Composite id='listHeaders' stretchX top='prev()' height={this.listHeadingFont.viewHeight}>
-            <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='TeamName'/>
-            <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='HomeTown'/>
-            <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='League'/>
-        </Composite>
-        <ListView id='teamsList' class='notWaitMode' stretchX bottom='next()' top='prev()' items={this.teams}>
-          <Cell highlightOnTouch onTap={
-            (ev) => {
-              console.log('click Team');
-              console.log(ev.target);
-              this._openTeamPopover(ev);
-            }
-          }>
-            <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.TeamName'/>
-            <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.HomeTown'/>
-            <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.League.LeagueName'/>
-          </Cell>
-        </ListView>
-      </$>
-    );
-    // have to enable waitMode after we have created the widgets
-    this.toggleWaitMode(true);
-    // assign ListView for easier handling
-    this._teamsListView = this._find(ListView).filter('#teamsList').only();
-  }
-
-  public setModelList(list: List<Team>): void {
-    this.toggleWaitMode(false);
-    this.teams = list;
-    // have to update ListView items because assignment destroys the object?
-    this._teamsListView.items = this.teams;
-  }
-
-  public toggleWaitMode(state?: boolean): void {
-    if (state === undefined) {
-        this.waitMode = !this.waitMode;
-    } else {
-        this.waitMode = state;
+        this.append(
+            <$>
+                <ActivityIndicator id='activity' class='waitMode' visible={false} center/>
+                <TextView id='heading' stretchX top={0} height={fonts.largeBold.viewHeight} font={fonts.largeBold} alignment='left'>NFFL Teams</TextView>
+                <Composite id='listHeaders' stretchX top='prev()' height={this.listHeadingFont.viewHeight}>
+                        <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='TeamName'/>
+                        <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='HomeTown'/>
+                        <TextView centerY left='prev()' right='next()' height={this.listHeadingFont.viewHeight} font={this.listHeadingFont} alignment='left' textColor='#212121' text='League'/>
+                </Composite>
+                <ListView id='teamsList' class='notWaitMode' stretchX bottom='next()' top='prev()' items={this.teams}>
+                    <Cell highlightOnTouch onTap={
+                        (ev) => {
+                            console.log('click Team');
+                            console.log(ev.target);
+                            this._openTeamPopover(ev);
+                        }
+                    }>
+                        <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.TeamName'/>
+                        <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.HomeTown'/>
+                        <TextView centerY left='prev()' right='next()' height={this.listFont.viewHeight} font={this.listFont} alignment='left' textColor='#212121' bind-text='item.League.LeagueName'/>
+                    </Cell>
+                </ListView>
+            </$>
+        );
+        // have to enable waitMode after we have created the widgets
+        this.toggleWaitMode(true);
+        // assign ListView for easier handling
+        this._teamsListView = this._find(ListView).filter('#teamsList').only();
     }
 
-    this._find('.waitMode').forEach(
-        (item) => item.visible = this.waitMode
-    );
-    this._find('.notWaitMode').forEach(
-        (item) => item.visible = !this.waitMode
-    );
+    public setModelList(list: List<Team>): void {
+        this.toggleWaitMode(false);
+        this.teams = list;
+        // have to update ListView items because assignment destroys the object?
+        this._teamsListView.items = this.teams;
+    }
+
+    public toggleWaitMode(state?: boolean): void {
+        if (state === undefined) {
+                this.waitMode = !this.waitMode;
+        } else {
+                this.waitMode = state;
+        }
+
+        this._find('.waitMode').forEach(
+                (item) => item.visible = this.waitMode
+        );
+        this._find('.notWaitMode').forEach(
+                (item) => item.visible = !this.waitMode
+        );
 }
 
-  public refreshteams() {
-    this.toggleWaitMode(true);
-    Team.listFactory.constructToView("", this, Team);
-  }
+    public refreshteams() {
+        this.toggleWaitMode(true);
+        Team.listFactory.constructToView("", this, Team);
+    }
 
-  private _openTeamPopover(ev: WidgetTapEvent<Widget>) {
-    console.log("function open Team popover");
-    const teamIndex: number = this._teamsListView.itemIndex(ev.target);
-    const team: Team = this.teams[teamIndex];
-    const teamView: TeamView = <TeamView stretch/>;
-    Team.factory.constructToView(team, teamView, Team);
-    TeamStats.factory.constructToView(team.TeamId, teamView, TeamStats);
-    const popover = Popover.open(
-      <Popover>
-        <NavigationView stretch>
-          <Action placement='navigation' title='Close' onSelect={() => popover.close()}/>
-          <Page title={team.Identification}/>
-        </NavigationView>
-      </Popover>
-    );
-    let page = popover.contentView.find(Page).only();
-    teamView.appendTo(page);
-  }
+    private _openTeamPopover(ev: WidgetTapEvent<Widget>) {
+        console.log("function open Team popover");
+        const teamIndex: number = this._teamsListView.itemIndex(ev.target);
+        const team: Team = this.teams[teamIndex];
+        const teamView: TeamView = <TeamView stretch/>;
+        Team.factory.constructToView(team, teamView, Team);
+        TeamStats.factory.constructToView(team.TeamId, teamView, TeamStats);
+        const popover = Popover.open(
+            <Popover>
+                <NavigationView stretch>
+                    <Action placement='navigation' title='Close' onSelect={() => popover.close()}/>
+                    <Page title={team.Identification}/>
+                </NavigationView>
+            </Popover>
+        );
+        let page = popover.contentView.find(Page).only();
+        teamView.appendTo(page);
+    }
 }
